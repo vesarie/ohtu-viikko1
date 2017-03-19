418041732322a0ebba363b42c349622965609fff
Matti Riekkinen <matti.riekkinen@icloud.com>
2017-03-19 16:01:55 +0200

    Lisätään tiedostoja alkuperäiseen paikalliseen kopioon

---
+++ io/BoardReaderException.java
@@ -0,0 +1,13 @@
+package sudoku.io;
+
+public class BoardReaderException extends Exception {
+
+    public BoardReaderException(String message) {
+        super(message);
+    }
+
+    public BoardReaderException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
